aa97df9f520929e8c7b23ee2f0ebafd4194ec0a8
Topper <serhii.hruntkovskii@gmail.com>
2023-07-29 16:20:15 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function reverse (n) {
-  
+    let reversedNumber = '';
+    const nLength = String(n).length;
+    const nString = String(n);
+
+    function counter () {
+        let i = nLength;
+        while (i > 0) {
+        reversedNumber = reversedNumber + nString[i - 1];
+        i = i - 1;
+        }
+    }
+
+    if (n < 0) {
+        counter();
+        reversedNumber = reversedNumber.slice(0, reversedNumber.length - 1);
+    }
+    else {
+        counter();
+    }
+
+    return reversedNumber;
 }
